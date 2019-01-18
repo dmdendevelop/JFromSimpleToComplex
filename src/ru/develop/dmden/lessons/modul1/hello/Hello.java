8a8f80af00bbd9813ce9976404e77211e9a6e240
dmdendevelop <dmden.develop@yandex.ru>
2019-01-18 13:50:06 +1000

    Operators

--- src/ru/develop/dmden/lessons/modul1/hello/Hello.java
+++ src/ru/develop/dmden/lessons/modul1/hello/Hello.java
@@ -1,6 +1,6 @@
-package ru.develop.dmden.lessons.modul1;
+package ru.develop.dmden.lessons.modul1.hello;
 
-public class App001 {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!!!");
     }
