cfd103d00ee98d72d52b633a779d078c5f790563
dmdendevelop <dmden.develop@yandex.ru>
2019-01-21 14:12:10 +1000

    OOP_1. Static.

--- src/ru/develop/dmden/lessons/modul3/oop/App.java
+++ src/ru/develop/dmden/lessons/modul3/oop/App.java
@@ -12,27 +12,33 @@ public class App {
         lab.setTail(1);
         lab.setBreed("Labrador");
         lab.setName("Dick");
+        lab.setSize("AVERAGE");
         System.out.println("=======================");
+        lab.bark();
+        lab.bite();
 
-        System.out.println("**********************");
-        System.out.println(lab.getName());
-        System.out.println(lab.getBreed());
-        System.out.println(lab.getPaws());
-        System.out.println(lab.getTail());
-        System.out.println("**********************");
+//        System.out.println("**********************");
+//        System.out.println(lab.getName());
+//        System.out.println(lab.getBreed());
+//        System.out.println(lab.getPaws());
+//        System.out.println(lab.getTail());
+//        System.out.println("**********************");
 
         Dog shepherd = new Dog();
         shepherd.setName("Mike");
         shepherd.setBreed("Shepherd");
-        shepherd.setTail(2);
-        shepherd.setPaws(5);
+        shepherd.setTail(1);
+        shepherd.setPaws(4);
+        shepherd.setSize("Large");
         System.out.println("========================");
+        shepherd.bark();
+        shepherd.bite();
 
-        System.out.println("**********************");
-        System.out.println(shepherd.getName());
-        System.out.println(shepherd.getBreed());
-        System.out.println(shepherd.getPaws());
-        System.out.println(shepherd.getTail());
-        System.out.println("**********************");
+//        System.out.println("**********************");
+//        System.out.println(shepherd.getName());
+//        System.out.println(shepherd.getBreed());
+//        System.out.println(shepherd.getPaws());
+//        System.out.println(shepherd.getTail());
+//        System.out.println("**********************");
     }
 }
--- src/ru/develop/dmden/lessons/modul3/oop/Dog.java
+++ src/ru/develop/dmden/lessons/modul3/oop/Dog.java
@@ -1,10 +1,40 @@
 package ru.develop.dmden.lessons.modul3.oop;
 
 public class Dog {
+    public static int dogsCounter;
+
     private int paws;
     private int tail;
     private String name;
     private String breed;
+    private String size;
+
+    public Dog() {
+        dogsCounter++;
+        System.out.println("Dogscounter = " + dogsCounter);
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if (size.equalsIgnoreCase("Small") ||
+                size.equalsIgnoreCase("average") ||
+                size.equalsIgnoreCase("large")) {
+            this.size = size;
+        } else {
+            System.out.println("Wrong size!!!");
+        }
+    }
+
+    public static int getDogsCounter() {
+        return dogsCounter;
+    }
+
+    public static void setDogsCounter(int dogsCounter) {
+        Dog.dogsCounter = dogsCounter;
+    }
 
     public int getPaws() {
         return paws;
@@ -46,5 +76,23 @@ public class Dog {
 
     public void setBreed(String breed) {
         this.breed = breed;
+    }
+
+    public void bark() {
+        if (size.equalsIgnoreCase("Large")) {
+            System.out.println("Wuf - wuf");
+        } else if (size.equalsIgnoreCase("average")) {
+            System.out.println("Raw - raw");
+        } else if (size.equalsIgnoreCase("small")) {
+            System.out.println("Tyaf - tyaf");
+        }
+    }
+
+    public void bite() {
+        if (dogsCounter > 2) {
+            System.out.println("Dogs bite!!!");
+        } else {
+            bark();
+        }
     }
 }
