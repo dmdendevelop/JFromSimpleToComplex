1b685dabb0cda6337522c70f657af9f816156f5b
dmdendevelop <dmden.develop@yandex.ru>
2019-01-23 13:51:41 +1000

    Inheritance_2_2.

--- src/ru/develop/dmden/lessons/modul4/App.java
+++ src/ru/develop/dmden/lessons/modul4/App.java
@@ -2,10 +2,6 @@ package ru.develop.dmden.lessons.modul4;
 
 public class App {
     public static void main(String[] args) {
-        Bus bus1 = new Bus("Kia", "GranBird", EngineType.DIESEL);
-        System.out.println(bus1);
-        bus1.toStart();
-        bus1.toAccelerate(60);
-        bus1.toStop();
+        
     }
 }
