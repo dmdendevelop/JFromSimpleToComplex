7633dc6c60bdebad76ebd94953ad2dfad5c8a730
dmdendevelop <dmden.develop@yandex.ru>
2019-02-14 16:40:52 +1000

    IO_read.

--- src/ru/develop/dmden/lessons/modul7/io/IOApp.java
+++ src/ru/develop/dmden/lessons/modul7/io/IOApp.java
@@ -2,13 +2,15 @@ package ru.develop.dmden.lessons.modul7.io;
 
 import ru.develop.dmden.lessons.modul6.sets.Car;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class IOApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Set<Car> cars1 = new HashSet<>();
         cars1.add(new Car("Mazda", "Atenza", 50));
         cars1.add(new Car("Audi", "A6", 70));
@@ -31,6 +33,18 @@ public class IOApp {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        //----------------------------------------------------------------------
+//        FileReader reader = new FileReader("Cars.txt");
+//        int c;
+//        while ((c = reader.read()) != -1) {
+//            System.out.println(c);
+//        }
+
+        BufferedReader reader = new BufferedReader(new FileReader("Document.txt"));
+        String c;
+        while ((c = reader.readLine()) != null) {
+            System.out.println(c);
         }
     }
 }
