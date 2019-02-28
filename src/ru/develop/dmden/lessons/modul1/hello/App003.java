e65a4ee17111e6630cdc29edf2330a4934e05148
dmdendevelop <dmden.develop@yandex.ru>
2019-02-28 13:15:32 +1000

    Formatter3.

---
+++ src/ru/develop/dmden/lessons/modul1/hello/App003.java
@@ -0,0 +1,7 @@
+package ru.develop.dmden.lessons.modul1.hello;
+
+public class App003 {
+    public static void main(String[] args) {
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+    }
+}
